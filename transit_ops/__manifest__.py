b0df012671d82234bbee134b1bc9ba76cf9afacf
shiiikha08 <shikharaghuwansi@gmail.com>
2026-07-12 12:50:42 +0530

    Added maintenance access security

--- transit_ops/__manifest__.py
+++ transit_ops/__manifest__.py
@@ -7,3 +7,18 @@
     "installable": True,
     "application": True
 }
+{
+    "name": "Transit Operations",
+    "version": "1.0",
+
+    "depends": [
+        "base"
+    ],
+
+    "data": [
+        "security/ir.model.access.csv"
+    ],
+
+    "installable": True,
+    "application": True
+}
